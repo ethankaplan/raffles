7c8f443fde5da8f7e62535cd03a64d49d3ea1622
ethankaplan <phaladone@gmail.com>
2020-07-09 15:09:00 -0700

    Better login org

--- frontend/src/component/Login/Enter.js
+++ frontend/src/component/Login/Enter.js
@@ -1,36 +1,47 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom'
-import {Grid, GridColumn,Container} from 'semantic-ui-react'
+import {Menu, GridColumn,Segment} from 'semantic-ui-react'
 
 import './Enter.css'
 
 class Enter extends Component{
-    state={
+    state = { activeItem: 'Register' }
 
-    }
-
-    render(){
-
-        return(
-            <div class="mainbox">
-            <Container>
-                <Grid textAlign='center' celled>
-                    <Grid.Row>
-                    <Grid.Column  width={8}>
-                        <p>REGISTER</p>
-                    </Grid.Column>
-                    <Grid.Column  width={8}>
-                        <p>LOGIN</p>
-                    </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-
-
-
-            </Container>
-            </div>
-
-        )
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  
+    render() {
+      const { activeItem } = this.state
+  
+      return (
+        <div class="mainbox">
+          <Menu attached='top' tabular widths={2}>
+            <Menu.Item
+              name='Register'
+              active={activeItem === 'Register'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='Log In'
+              active={activeItem === 'Log In'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
+  
+          <Segment attached='bottom'>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly believable.
+            If you are going to use a passage of Lorem Ipsum, you need to be sure
+            there isn't anything embarrassing hidden in the middle of text. All
+            the Lorem Ipsum generators on the Internet tend to repeat predefined
+            chunks as necessary, making this the first true generator on the
+            Internet. It uses a dictionary of over 200 Latin words, combined with
+            a handful of model sentence structures, to generate Lorem Ipsum which
+            looks reasonable. The generated Lorem Ipsum is therefore always free
+            from repetition, injected humour, or non-characteristic words etc.
+          </Segment>
+        </div>
+      )
     }
 }
 export default Enter
